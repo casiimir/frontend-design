f748d4f4726acd23aad11563f47735664b7c267f
casiimir <cp.ciancimino@gmail.com>
2026-02-18 17:48:06 +0100

    fix: css variables consistent to epicode.com + favicon

--- packages/design-system/src/lib/utils.ts
+++ packages/design-system/src/lib/utils.ts
@@ -1,7 +1,5 @@
 import { cn as primitiveCn } from "@workspace/ui/lib/utils";
 
-const WHITESPACE_REGEX = /\s+/;
-
 export function cn(...inputs: Parameters<typeof primitiveCn>) {
   return primitiveCn(...inputs);
 }
