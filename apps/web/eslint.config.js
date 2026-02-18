a8f8446eaf5b49d0fec6aa8a9a71fde6a14d0631
casiimir <cp.ciancimino@gmail.com>
2026-02-18 17:35:21 +0100

    chore: config postcss + turbo + minor linting elements

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,7 +1,9 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import { nextJsConfig } from "@workspace/eslint-config/next-js";
 
-/** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  {
+    ignores: [".next/**", "node_modules/**", "dist/**"],
+  },
+  ...nextJsConfig,
+];
